2833b829be9c91bc2d7dada20b0cfa82a674d179
soponpunth <peach.ronakorn@gmail.com>
2023-11-16 00:26:12 +0100

    Create AutoPass.py
    added script

---
+++ AutoPass.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+import winsound
+import pyautogui
+import time
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+# https://github.com/UB-Mannheim/tesseract/wiki
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+
+
+###############################
+# CONST
+###############################
+SCREEN_WIDTH = 3840
+SCREEN_HEIGHT = 2160
+
+
+COMBAT_OFFSET = (int(SCREEN_WIDTH/2) - 420, int(SCREEN_HEIGHT * 0.85), 820, 60)
+END_OFFSET = (int(SCREEN_WIDTH/2) - 220, int(SCREEN_HEIGHT * 0.85), 460, 60)
+
+
+GRACE_PERIOD = 5
+WAVE_RUNTIME = 65
+SOUND_HZ = 440
+SOUND_MS = 6000
+
+DEBUG = False
+
+
+def extract_bright_map(offset, error = 5):
+
+    for i in range(5):
+
+        time.sleep(0.2)
+        im = pyautogui.screenshot(region=offset)
+
+        # erosion -> dilation
+        im = im.filter(ImageFilter.MinFilter(3))    
+        im = im.filter(ImageFilter.MaxFilter(3))
+        
+        # enhance different between text and background
+        enhancer = ImageEnhance.Contrast(im)
+        im = enhancer.enhance(2)
+
+        # enhance text's edges
+        sharper = ImageEnhance.Sharpness(im)
+        im = sharper.enhance(1)
+        
+        # enhance text color quality
+        colorer = ImageEnhance.Color(im)
+        im = colorer.enhance(2)
+
+        # to gray scale
+        im = im.convert("1")
+        text = ""
+        if DEBUG:
+            im.save(f"temp{i}.jpg")
+            text = pytesseract.image_to_string(Image.open(f"temp{i}.jpg"))
+        else:
+            text = pytesseract.image_to_string(im)
+
+        text = text.strip()
+        print(f"Detected: {text}")
+        if len(text) >= error:
+            return True
+
+    return False
+
+
+def wait_and_combat():
+    for _ in range(20):    
+        found = extract_bright_map(COMBAT_OFFSET)
+
+        if found:
+            print("Pressing G")
+            pyautogui.press("g")
+            return
+
+        print(f"Waiting for G button...")
+        time.sleep(5)
+
+
+def end_wave():
+    for _ in range(10):
+        found = extract_bright_map(END_OFFSET)
+
+        if found:
+            print("Pressing G for ending wave")
+            winsound.Beep(SOUND_HZ, SOUND_MS)
+            time.sleep(1)
+            pyautogui.press("g")
+            return
+
+        print(f"Waiting for G button to end round...")
+        time.sleep(5)
+
+def main_combat(waves):
+    print(f"Starting in...")
+    for i in range(5, -1, -1):
+        print(i)
+        time.sleep(1)
+
+    for w in range(waves):
+        wait_and_combat()
+
+        print(f"Running Wave: {w+1} of {waves}")
+
+        # wait for the wave to end
+        for i in range(WAVE_RUNTIME):
+            if i % 5 == 0:
+                print(f"Waiting {i} of {WAVE_RUNTIME} seconds")
+            time.sleep(1)
+
+        # wait for wave end animation   
+        time.sleep(GRACE_PERIOD)
+
+    end_wave()
+    waves = input("Restart Script? Re-enter number of waves: ")
+    if len(waves) > 0:
+        main_combat(int(waves))
+    else:
+        print("Bye...")
+        exit(0)
+
+
+if __name__ == "__main__":
+    waves = input("Enter number of waves: ")
+    main_combat(int(waves))
